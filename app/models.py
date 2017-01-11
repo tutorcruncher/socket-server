3c84d8656d5efba95bdac4f6e72feb35743dde05
Samuel Colvin <s@muelcolvin.com>
2017-01-11 22:00:09 +0000

    improving db models

--- app/models.py
+++ app/models.py
@@ -1,16 +1,46 @@
-from sqlalchemy import Column, Float, Integer, Sequence, String
-from sqlalchemy import ForeignKey
-from sqlalchemy import ForeignKeyConstraint
+from enum import Enum, unique
+
+from sqlalchemy import Column, Float, ForeignKey, Integer, PrimaryKeyConstraint, Sequence, String
+from sqlalchemy import Enum as _SAEnum
 from sqlalchemy.dialects.postgresql import JSONB
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 
 
+@unique
+class NameOptions(str, Enum):
+    first_name = 'first_name'
+    first_name_initial = 'first_name_initial'
+    full_name = 'full_name'
+
+
+
+def sa_enum(enum: Enum):
+    return _SAEnum(*(v.value for v in enum.__members__.values()), name=enum.__name__.lower())
+
+
+class Company(Base):
+    __tablename__ = 'companies'
+    # id set from profile id on TutorCruncher
+    id = Column(Integer, primary_key=True, nullable=False)
+    key = Column(String(15), nullable=False, unique=True)
+
+    name = Column(String(63))
+    site_domain = Column(String(127))
+
+    name_display = Column(sa_enum(NameOptions))
+
+
+sa_companies = Company.__table__
+
+
 class Contractor(Base):
     __tablename__ = 'contractors'
     # id set from profile id on TutorCruncher
     id = Column(Integer, primary_key=True, nullable=False)
+    company = Column(Integer, ForeignKey('companies.id'), nullable=False)
+
     first_name = Column(String(63))
     last_name = Column(String(63), nullable=False)
 
@@ -18,6 +48,8 @@ class Contractor(Base):
     country = Column(String(63))
     latitude = Column(Float())
     longitude = Column(Float())
+
+    tag_line = Column(String(63))
 
     extra_attributes = Column(JSONB)
     image = Column(String(63))
@@ -49,19 +81,14 @@ sa_qual_levels = QualLevel.__table__
 
 
 class ConSkill(Base):
-    __tablename__ = 'con_skills'
+    __tablename__ = 'contractor_skills'
 
-    id = Column(Integer, Sequence('con_skill_id_seq'), primary_key=True, nullable=False)
     contractor = Column(Integer, ForeignKey('contractors.id'), nullable=False)
     subject = Column(Integer, ForeignKey('subjects.id'), nullable=False)
     qual_level = Column(Integer, ForeignKey('qual_levels.id'), nullable=False)
 
     __table_args__ = (
-        ForeignKeyConstraint(
-            ('contractor', 'subject', 'qual_level'),
-            ('contractors.id',),
-            name='_con_skills'
-        ),
+        PrimaryKeyConstraint('contractor', 'subject', 'qual_level', name='_con_skills'),
     )
 
 
--- app/views.py
+++ app/views.py
@@ -1,14 +1,6 @@
-from aiohttp.hdrs import METH_POST
-from aiohttp.web_exceptions import HTTPFound
 from aiohttp.web_reqrep import json_response
-from aiohttp_jinja2 import template
-
-from aiohttp_session import get_session
-
-# from .models import sa_messages
 
 
-@template('index.jinja')
 async def index(request):
     """
     This is the view handler for the "/" url.
@@ -16,65 +8,7 @@ async def index(request):
     :param request: the request object see http://aiohttp.readthedocs.io/en/stable/web_reference.html#request
     :return: context for the template.
     """
-    # Note: we return a dict not a response because of the @template decorator
-    return {
+    return json_response({
         'title': request.app['name'],
         'intro': "Success! you've setup a basic aiohttp app.",
-    }
-
-
-async def process_form(request):
-    new_message, missing_fields = {}, []
-    fields = ['username', 'message']
-    data = await request.post()
-    for f in fields:
-        new_message[f] = data.get(f)
-        if not new_message[f]:
-            missing_fields.append(f)
-
-    if missing_fields:
-        return 'Invalid form submission, missing fields: {}'.format(', '.join(missing_fields))
-
-    # simple demonstration of sessions by saving the username and pre-populating it in the form next time
-    session = await get_session(request)
-    session['username'] = new_message['username']
-
-    async with request.app['pg_engine'].acquire() as conn:
-        await conn.execute(sa_messages.insert().values(
-            username=new_message['username'],
-            message=new_message['message'],
-        ))
-    raise HTTPFound(request.app.router['messages'].url())
-
-
-@template('messages.jinja')
-async def messages(request):
-    if request.method == METH_POST:
-        # the 302 redirect is processed as an exception, so if this coroutine returns there's a form error
-        form_errors = await process_form(request)
-    else:
-        form_errors = None
-
-    # simple demonstration of sessions by pre-populating username if it's already been set
-    session = await get_session(request)
-    username = session.get('username', '')
-
-    return {
-        'title': 'Message board',
-        'form_errors': form_errors,
-        'username': username,
-    }
-
-
-async def message_data(request):
-    """
-    As an example of aiohttp providing a non-html response, we load the actual messages for the "messages" view above
-    via ajax using this endpoint to get data. see static/message_display.js for details of rendering.
-    """
-    messages = []
-
-    async with request.app['pg_engine'].acquire() as conn:
-        async for row in conn.execute(sa_messages.select().order_by(sa_messages.c.timestamp.desc())):
-            ts = '{:%Y-%m-%d %H:%M:%S}'.format(row.timestamp)
-            messages.append({'username': row.username, 'timestamp':  ts, 'message': row.message})
-    return json_response(messages)
+    })
