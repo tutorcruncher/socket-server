e929b54e4c0ae387d46d6093d51f10163de203c7
Samuel Colvin <s@muelcolvin.com>
2017-02-09 18:05:24 +0000

    adding shell command

--- tcsocket/run.py
+++ tcsocket/run.py
@@ -149,6 +149,40 @@ def resetdb(*, no_input, **kwargs):
     prepare_database(delete, print_func=logger.info)
 
 
+EXEC_LINES = [
+    'import asyncio, os, re, sys',
+    'from datetime import datetime, timedelta, timezone',
+    'from pprint import pprint as pp',
+    '',
+    'from aiopg.sa import create_engine',
+    'from app.settings import load_settings',
+    'from app.main import pg_dsn',
+    'from app.models import sa_companies, sa_contractors, sa_subjects, sa_qual_levels, sa_con_skills',
+    '',
+    'loop = asyncio.get_event_loop()',
+    'await_ = loop.run_until_complete',
+    'settings = load_settings()',
+    'pg = await_(create_engine(pg_dsn(settings[\'database\'])))',
+    'conn = await_(pg._acquire())',
+]
+EXEC_LINES += (
+    ['print("\\n    Python {v.major}.{v.minor}.{v.micro}\\n".format(v=sys.version_info))'] +
+    [f'print("    {l}")' for l in EXEC_LINES]
+)
+
+
+@command
+def shell(**kwargs):
+    from IPython import start_ipython
+    from IPython.terminal.ipapp import load_default_config
+    c = load_default_config()
+
+    c.TerminalIPythonApp.display_banner = False
+    c.TerminalInteractiveShell.confirm_exit = False
+    c.InteractiveShellApp.exec_lines = EXEC_LINES
+    start_ipython(argv=(), config=c)
+
+
 @click.command()
 @click.argument('command', type=click.Choice([c.__name__ for c in commands]))
 @click.option('--no-input', is_flag=True)
