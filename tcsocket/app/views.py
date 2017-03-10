931f00573aef9de92f28f8f3ab806b2fbb152c57
Samuel Colvin <s@muelcolvin.com>
2017-03-10 13:07:45 +0000

    allow longer recaptcha

--- tcsocket/app/views.py
+++ tcsocket/app/views.py
@@ -80,8 +80,8 @@ VIEW_SCHEMAS = {
         # t.Key('subject', optional=True): t.Or(t.Null | t.Int(gt=0)),
         # t.Key('qual_level', optional=True): t.Or(t.Null | t.Int(gt=0)),
 
-        t.Key('upstream_http_referrer', optional=True): t.Or(t.Null | t.String(max_length=200)),
-        t.Key('grecaptcha_response', optional=True): t.Or(t.Null | t.String(max_length=400)),
+        t.Key('upstream_http_referrer', optional=True): t.Or(t.Null | t.String(max_length=255)),
+        t.Key('grecaptcha_response', optional=True): t.Or(t.Null | t.String(min_length=20, max_length=1000)),
     })
 }
 VIEW_SCHEMAS['contractor-set'].ignore_extra('*')
