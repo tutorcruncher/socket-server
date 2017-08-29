6e719e7ae4d1c2c3207813b6d7ceb519ee05384a
Samuel Colvin <s@muelcolvin.com>
2017-08-29 10:53:29 +0100

    uprev nginx, tweaking compose definitions

--- activate.dev.sh
+++ activate.dev.sh
@@ -2,7 +2,6 @@
 . env/bin/activate
 export COMMIT=`git rev-parse HEAD`
 export RELEASE_DATE="<not set>"
-export SERVER_NAME="localhost"
 export APP_MASTER_KEY="testing"
 export CLIENT_SIGNING_KEY="testing"
 export RAVEN_DSN="-"
--- start-prod.sh
+++ start-prod.sh
@@ -3,4 +3,4 @@
 gnome-terminal \
     --geometry=180x40 \
     --tab-with-profile=prod \
-    -x bash -c "cd $(dirname "$0"); source activate.prod.sh; docker ps; exec bash"
+    -x bash -c "cd $(dirname "$0"); exec bash --rcfile ${1:-activate.prod.sh}"
